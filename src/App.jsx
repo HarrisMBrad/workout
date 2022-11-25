9493b173daa611ed157d7bc52bb34dfb1678913a
Brad M. Harris <bmichaelh13@gmail.com>
2022-11-25 18:00:22 +0000

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import * as React from 'react';
+
+//import Constants from 'expo-constants';
+//import Screens from './components/Screens';
+//import { Card } from 'react-native-paper';
 
 export default function App() {
-  return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+	return (
+
+		<main>
+			React ⚛️ + Vite ⚡ + Replit 🌀
+		</main>
+
+
+	);
 }
